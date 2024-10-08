ddb27656d41fc4c43af70a5f6bb3ab18dfefdf33
Akhil Chitarkhot <146656862+Akhil-Chitarkhot@users.noreply.github.com>
2024-10-08 19:23:40 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup>
+import { ref } from 'vue'
+
+
+const count = ref(0)
+
+</script>
+
+<template>
+  <div class="flex items-center justify-center min-h-screen">
+    <div class="card bg-neutral text-neutral-content w-80 h-80">
+      <div class="card-body justify-center items-center">
+        <div>
+          <h2 class="card-title pb-4">Counter</h2>
+          <p>Current Count : </p>
+          <p class="pt-4"> {{ count }} </p>
+        </div>
+        <div class="card-actions pt-8">
+          <button class="btn btn-success" @click = "count++">Add</button>
+          <button class="btn btn-error" @click = "count--">Subtract</button>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+
+
+
+
+
+</template>
+
+
